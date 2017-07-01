b2adfd18812b8175ccdc30b57ea4c1b2515d9f08
Ivan Doroshenko <ivan.doroshenko@gettyimages.com>
2017-07-01 16:15:29 +0800

    .gitignore

--- src/register.js
+++ src/register.js
@@ -1,43 +1,47 @@
-var Register = function(){
-    this.classes = {};
-    this.injections = {};
-};
+const RegisterClass = require('./registerclass')
+
+const Register = function(){
+  this.classes = {}
+  this.injections = {}
+}
 Register.prototype = {
-    registerClass : function(name, func, injections){
-        this.classes[name] = this.injections[name] = new RegisterClass(func, injections, this.injections);
-        return this;
-    },
-    registerClasses : function(){
-        var args = Array.prototype.slice.call(arguments);
-        for(var i = 0, ln = args.length; i < ln; i++){
-            this.registerClass(args[i][0], args[i][1], args[i][2]);
-        }
-        return this;
-    },
-    registerInjection : function(name, value){
-        this.injections[name] = value;
-        return this;
-    },
-    registerInjections : function(injections){
-        if (Array.isArray(injections)) {
-            var args = Array.prototype.slice.call(arguments);
-            for(var i = 0, ln = args.length; i < ln; i++){
-                this.registerInjection(args[i][0], args[i][1]);
-            }
-        } else {
-            for(var name in injections){
-                if(injections.hasOwnProperty(name)){
-                    this.registerInjection(name, injections[name]);
-                }
-            }
-        }
-        return this;
-    },
-    getInstance : function(){
-        var args = Array.prototype.slice.call(arguments, 1);
-        return this.classes[arguments[0]].getInstance(args);
-    },
-    getInjection: function(name){
-        return this.injections[name];
+  registerClass : function(name, func, injections){
+    this.classes[name] = this.injections[name] = new RegisterClass(func, injections, this.injections)
+    return this
+  },
+  registerClasses : function(){
+    const args = Array.prototype.slice.call(arguments)
+    for(let i = 0, ln = args.length; i < ln; i++){
+      this.registerClass(args[i][0], args[i][1], args[i][2])
     }
-};
+    return this
+  },
+  registerInjection : function(name, value){
+    this.injections[name] = value
+    return this
+  },
+  registerInjections : function(injections){
+    if (Array.isArray(injections)) {
+      const args = Array.prototype.slice.call(arguments)
+      for(let i = 0, ln = args.length; i < ln; i++){
+        this.registerInjection(args[i][0], args[i][1])
+      }
+    } else {
+      for(let name in injections){
+        if(injections.hasOwnProperty(name)){
+          this.registerInjection(name, injections[name])
+        }
+      }
+    }
+    return this
+  },
+  getInstance : function(){
+    const args = Array.prototype.slice.call(arguments, 1)
+    return this.classes[arguments[0]].getInstance(args)
+  },
+  getInjection: function(name){
+    return this.injections[name]
+  }
+}
+
+module.exports = Register
--- src/registerclass.js
+++ src/registerclass.js
@@ -1,82 +1,85 @@
-var RegisterClass = function RegisterClass(func, injections, globalInjections){
-    this.static = RegisterClass;
-    this._func = func;
-    this._ownInjections = injections||{};
-    this._globalInjections = globalInjections||{};
-    this._args = this.extractArgs(this._func.toString());
-    this._argsLn = this._args.length;
-};
+const RegisterClass = function RegisterClass(func, injections, globalInjections){
+  this.static = RegisterClass
+  this._func = func
+  this._ownInjections = injections||{}
+  this._globalInjections = globalInjections||{}
+  this._args = this.extractArgs(this._func.toString())
+  this._argsLn = this._args.length
+}
 RegisterClass.prototype = {
-    /*
-    * thanks to angularjs team
-    * */
-    FN_ARGS : /^function\s*[^\(]*\(\s*([^\)]*)\)/m,
-    FN_ARG_SPLIT : /\s*,\s*/,
-    FN_ARG : /^\s*(_?)(\S+?)\1\s*$/,
-    STRIP_COMMENTS : /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg,
-    global : window,
-    undefined : void 0,
-    extractArgs : function(source){
-        var args = [],
-            fnText = source.replace(this.STRIP_COMMENTS, ''),
-            argDecl = fnText.match(this.FN_ARGS);
-        argDecl && argDecl[1] && argDecl[1].split(this.FN_ARG_SPLIT).forEach(function(arg){
-            arg.replace(this.FN_ARG, function(all, underscore, name){
-                args.push(name.indexOf('$') === 0
-                    ? name.substr(1)
-                    : null);
-            });
-        }.bind(this));
-        return args;
-    },
-    getInstance : function(args){
-        return new (this.getConstructor(args));
-    },
-    getType : function(){
-        return this._func;
-    },
-    getConstructor : function(args, ctx){
-        var injections = this.extractInjections();
-        for(var i = 0; args && args[0]; i++){
-            if(injections[i] === this.undefined){
-                injections[i] = args.shift();
-            }
-        }
-        return (Function.prototype.bind.apply(this._func, [ctx||null].concat(injections)));
-    },
-    _extract : function(str){
-        try{
-            var target = this.global;
-            str.split('.').forEach(function(scope){
-                target = target[scope];
-            });
-            return target;
-        }catch(e){
-            return undefined;
-        }
-    },
-    extractInjections : function(){
-        var extractedInjections = [];
-        for(var i = 0, ln = this._args.length; i < ln; i++){
-            var name = this._args[i];
-            var entity = name
-                ? (this._ownInjections[name] || this._globalInjections[name] || this._extract(name) || null)
-                : this.undefined;
-            if(entity && entity instanceof this.static){
-                if (name[0].toUpperCase() === name[0]) {
-                    entity = (function Closure(rgClass) {
-                        return function DIContainer() {
-                            return this.constructor === DIContainer
-                                ? new (rgClass.getConstructor(Array.prototype.slice.call(arguments, 0)))
-                                : (rgClass.getConstructor(Array.prototype.slice.call(arguments, 0), this)).call(this);
-                        };
-                    })(entity);
-                }else {
-                    entity = entity.getInstance();
-                }
-            }
-            extractedInjections.push(entity);
-        }
-        return extractedInjections;
+  /*
+   * thanks to angularjs team
+   * */
+  FN_ARGS : /^function\s*[^\(]*\(\s*([^\)]*)\)/m,
+  FN_ARG_SPLIT : /\s*,\s*/,
+  FN_ARG : /^\s*(_?)(\S+?)\1\s*$/,
+  STRIP_COMMENTS : /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg,
+  global : global,
+  undefined : void 0,
+  extractArgs : function(source){
+    let args = [],
+      fnText = source.replace(this.STRIP_COMMENTS, ''),
+      argDecl = fnText.match(this.FN_ARGS)
+    argDecl && argDecl[1] && argDecl[1].split(this.FN_ARG_SPLIT).forEach(arg => {
+      arg.replace(this.FN_ARG, function(all, underscore, name){
+        args.push(name.indexOf('$') === 0
+          ? name.substr(1)
+          : null)
+      });
+    })
+    return args
+  },
+  getInstance : function(args){
+    return new (this.getConstructor(args))
+  },
+  getType : function(){
+    return this._func
+  },
+  getConstructor : function(args, ctx){
+    let injections = this.extractInjections();
+    for(let i = 0; args && args[0]; i++){
+      if(injections[i] === this.undefined){
+        injections[i] = args.shift()
+      }
     }
-};
+    return (Function.prototype.bind.apply(this._func, [ctx||null].concat(injections)))
+  },
+  _extract : function(str){
+    try{
+      let target = this.global
+      str.split('.').forEach(scope => {
+        target = target[scope]
+      })
+      return target
+    } catch(e) {
+      return undefined
+    }
+  },
+  extractInjections : function(){
+    let extractedInjections = []
+    for(let i = 0, ln = this._args.length; i < ln; i++){
+      let
+        name = this._args[i],
+        entity = name
+          ? (this._ownInjections[name] || this._globalInjections[name] || this._extract(name) || null)
+          : this.undefined
+      if(entity && entity instanceof this.static){
+        if (name[0].toUpperCase() === name[0]) {
+          entity = (rgClass => {
+            return function DIContainer() {
+              return this.constructor === DIContainer
+                ? new (rgClass.getConstructor(Array.prototype.slice.call(arguments, 0)))
+                : (rgClass.getConstructor(Array.prototype.slice.call(arguments, 0), this)).call(this)
+            }
+          })(entity)
+        } else {
+          entity = entity.getInstance()
+        }
+      }
+      extractedInjections.push(entity)
+    }
+    return extractedInjections
+  }
+}
+
+module.exports = RegisterClass
